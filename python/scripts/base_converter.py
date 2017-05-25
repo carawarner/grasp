c3063dd59c629b021dd9cdbfb8e005a04fb451b0
carawarner <warnercara@gmail.com>
2017-05-25 18:30:22 -0400

    Linted base converter: fixed indentations, docstrings, var names

--- python/scripts/base_converter.py
+++ python/scripts/base_converter.py
@@ -1,54 +1,56 @@
-from math import log, floor
-
 """Convert input into specified base. Assumes input is in base 10!
 
-   Imagine base notation represented by columns that are zero-indexed
-   from right-to-left where column i represents base ** i, like this:
+Imagine base notation represented by columns that are zero-indexed
+from right-to-left where column i represents base ** i, like this:
 
-   Column:    |   i    |   3    |   2    |   1    |   0    |
-   Exponent:  | 2 ** i | 2 ** 3 | 2 ** 2 | 2 ** 1 | 2 ** 0 |
-   Value:     | 2 ** i |   8    |   4    |   2    |   1    |
+Column:    |   i    |   3    |   2    |   1    |   0    |
+Exponent:  | 2 ** i | 2 ** 3 | 2 ** 2 | 2 ** 1 | 2 ** 0 |
+Value:     | 2 ** i |   8    |   4    |   2    |   1    |
 
-   Method uses floor(log(x/base)) to calculate i, then loops over the
-   "columns" from i to zero. On each loop, the method calculates what
-   digit(s) should be written in the column and how much of x remains
-   to be converted.
+Method uses floor(log(x/base)) to calculate i, then loops over the
+"columns" from i to zero. On each loop, the method calculates what
+digit(s) should be written in the column and how much of x remains
+to be converted.
 """
-def convert_to_base(x, base):
-  if base <= 0:
-    print "\nERROR - base must be greater than zero\n"
-    return
 
-  if x == 0:
-    return 0
+from math import log, floor
 
-  result = ''
-  remainder = abs(x)
-  exponent = int(floor(log(abs(x),base)))
+def convert_to_base(num, base):
+    """Takes a number in base 10, transforms it to some other base"""
 
-  while exponent >= 0:
-    units = base ** exponent
-    if remainder >= units:
-      (q, remainder) = divmod(remainder, units)
-      result = result + str(q)
-    else:
-      result = result + '0'
+    if base <= 0:
+        print "\nERROR - base must be greater than zero\n"
+        return
 
-    # Proceed to next column
-    exponent = exponent -1
+    if num == 0:
+        return 0
 
-  return -1 * int(result) if x < 0 else int(result)
+    result = ''
+    remainder = abs(num)
+    exponent = int(floor(log(abs(num), base)))
 
-# Provides a way to interact with base_converter from the CL
+    while exponent >= 0:
+        units = base ** exponent
+        if remainder >= units:
+            (quotient, remainder) = divmod(remainder, units)
+            result = result + str(quotient)
+        else:
+            result = result + '0'
+
+        # Proceed to next column
+        exponent = exponent -1
+
+    return -1 * int(result) if num < 0 else int(result)
+
 def try_base_converter():
-    x = input("\nWhat number would you like to convert? ")
+    """Provides a way to interact with base_converter from the CL"""
+    num = input("\nWhat number would you like to convert? ")
     base = input("Into which base? ")
-    result = convert_to_base(x,base)
+    result = convert_to_base(num, base)
 
     if isinstance(result, int):
-        print "\nIn base %d the number %d is represented by %d\n" % (base, x, result)
+        print "\nIn base %d the number %d is represented by %d\n" % (base, num, result)
     else:
         print "Result that came back wasn't an integer. Something went wrong."
 
 try_base_converter()
-
