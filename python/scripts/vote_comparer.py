fbd395df6db8033c65d7404287674be8059116b8
carawarner <warnercara@gmail.com>
2017-06-26 07:45:58 -0400

    Play with using the dot product to compare voting records.

--- python/scripts/vote_comparer.py
+++ python/scripts/vote_comparer.py
@@ -1,11 +1,11 @@
-# pylint: disable=relative-import
+# pylint: disable=relative-import,multiple-statements
 """Adapted from an exercise in "Coding the Matrix". This script ingests
 a text file, generates a dictionary containing the voting records of diff-
 erent senators, and surfaces methods for comparing those voting records.
-There is no validation.
+There is limited validation.
 """
 
-from vec import dot_prod
+from vec import dot_prod, vec_average
 
 def generate_voting_dict(strlist, index_of_first_vote):
     """Ingest a list of strings containing senators' names and voting
@@ -41,16 +41,15 @@ def most_similar(sen_a, voting_dict):
     most_similar_sen = None
     highest_score = -1 * len(voting_dict[sen_a]) #start with disagreeing on every vote
 
-    senators = voting_dict.keys()
-    senators.remove(sen_a)
+    sens_b = [sen for sen in voting_dict.keys() if not sen == sen_a]
 
-    for sen_b in senators:
+    for sen_b in sens_b:
         score = policy_compare(sen_a, sen_b, voting_dict)
         if score > highest_score:
             highest_score = score
             most_similar_sen = sen_b
 
-    print "\nThe closest match to Senator %s is Senator %s with match score %d \n" \
+    print "The closest match to Senator %s is Senator %s with match score %d \n" \
         % (sen_a, most_similar_sen, highest_score)
     return
 
@@ -62,23 +61,87 @@ def least_similar(sen_a, voting_dict):
     least_similar_sen = None
     lowest_score = len(voting_dict[sen_a]) #start with agreeing on every vote
 
-    senators = voting_dict.keys()
-    senators.remove(sen_a)
-
-    for sen_b in senators:
+    sens_b = [sen for sen in voting_dict.keys() if not sen == sen_a]
+    for sen_b in sens_b:
         score = policy_compare(sen_a, sen_b, voting_dict)
         if score < lowest_score:
             lowest_score = score
             least_similar_sen = sen_b
 
-    print "\nThe furthest match from Senator %s was Senator %s with match score %d \n" \
+    print "The furthest match from Senator %s was Senator %s with match score %d \n" \
         % (sen_a, least_similar_sen, lowest_score)
     return
 
 
-def list_names(voting_dict):
-    """Print a list of senators' names."""
-    print voting_dict.keys()
+def find_average_similarity(sen_a, sen_set, voting_dict):
+    """Compare sen_a's voting record against those of all senators in
+    sen_set and return his/her agreement with the average.
+    """
+    sum_of_all_scores = 0.
+
+    sens_b = [sen for sen in sen_set if not sen == sen_a]
+    for sen_b in sens_b:
+        sum_of_all_scores += policy_compare(sen_a, sen_b, voting_dict)
+
+    return sum_of_all_scores/len(sens_b)
+
+
+def find_average_similarity2(sen_a, sen_set, voting_dict):
+    """Same as find_average_similarity() only instead of taking the average
+    of the dot products, it takes the dot product of the average.
+    """
+    votes = [voting_dict[sen] for sen in sen_set if not sen == sen_a]
+    average = vec_average(votes)
+
+    return dot_prod(voting_dict[sen_a], average)
+
+
+def most_average(sen_set, voting_dict):
+    """Finds the senator whose voting record is closest to the average
+    of the records of all the sens in sen_set.
+    """
+    highest_score = 0
+    most_average_sen = None
+
+    for sen in sen_set:
+        score = find_average_similarity2(sen, sen_set, voting_dict)
+        print "%s vs the party: %d" % (sen, score)
+        if score > highest_score:
+            highest_score = score
+            most_average_sen = sen
+
+    return most_average_sen
+
+
+def most_average2(sen_set, voting_dict):
+    """Similar to most_average() but more efficient because it calculates
+    a single voting average and compares senators' records agains that.
+    Efficiency comes at a cost: this is less accurage for outliers."""
+    highest_score = 0
+    most_average_sen = None
+
+    votes = [voting_dict[sen] for sen in sen_set]
+    average = vec_average(votes)
+
+    for sen in sen_set:
+        score = dot_prod(voting_dict[sen], average)
+        if score > highest_score:
+            highest_score = score
+            most_average_sen = sen
+
+    return most_average_sen
+
+
+def extract_party_members(party):
+    """Read from the data dump that accompanied this exercise. Return a list of
+    senators who are members of the specified political party.
+    """
+    text = open('../src/voting_record_dump109.txt')
+    strlist = text.readlines()
+    records = [strlist[i].split(' ') for i in range(len(strlist))]
+    sens = [records[i][0] for i in range(len(records)) if records[i][1] == party]
+
+    return sens
 
 
 def dict_from_file():
